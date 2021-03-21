699afd33a369fd0d871a2aa32ce3bbe023e9d8d5
tstih <tomaz.stih@post.com>
2021-03-21 10:57:37 +0000

    Added google test.

--- build/parse_tests_include-b12d07c.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/tomaz/Work/lingo/build/parse_tests_tests-b12d07c.cmake")
-  include("/home/tomaz/Work/lingo/build/parse_tests_tests-b12d07c.cmake")
-else()
-  add_test(parse_tests_NOT_BUILT-b12d07c parse_tests_NOT_BUILT-b12d07c)
-endif()
